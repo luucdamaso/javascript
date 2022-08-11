20be1fe1872e6bc195a4554a2440d99331f3ed8b
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-11 16:27:02 -0300

    Update ex012.js

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -1,11 +1,11 @@
 var agora = new Date()
 var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora}horas`)
+console.log(`Agora são exatamente ${hora} horas`)
 if (hora < 12) {
     console.log('Bom dia')
 } else if (hora <= 18) {
     console.log('Boa tarde')
-} else if (hora <= 0) {
+} else if (hora <= 23) {
     console.log('Boa noite')
 } else if (hora <= 6){
     console.log('Boa madrugada')
