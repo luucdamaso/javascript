e16b9eb681b903e238b9e431e59d597e908b9a17
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-11 18:49:06 -0300

    crianças da pasta imagens

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -5,13 +5,13 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 6  && hora < 12) {
-        img.src = 'manha.jpg'
+        img.src = 'imagens/manha.jpg'
         document.body.style.background = '#f4d2c6'
     } else if ( hora >= 12 && hora <= 18) {
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#d9680c'
     } else {
-        img.src ='noite.jpg'
+        img.src ='imagens/noite.jpg'
         document.body.style.background = '#07418a'
     }
 }
