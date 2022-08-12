ba09a4a8725ec1191321d0f23b60a6e5eaf78ace
luucdamaso <107771309+luucdamaso@users.noreply.github.com>
2022-08-12 10:48:42 -0300

    aula 013

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,18 +1,11 @@
-var c = 1
-do {
-    console.log(`Passo ${c}`)
-    c++
-} while (c <= 6)
-
 /*var c = 1
 while (c <= 6) {
     console.log('Tudo bem?')
     c++
 }*/
 
-/*console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')*/
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
